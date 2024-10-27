24faac2e1f41655a83f77943f5962c112cdd1940
ngcarvajall <ngcarvajall@gmail.com>
2024-10-27 15:14:49 +0100

    conclusiones

--- src/funciones.py
+++ src/funciones.py
@@ -7,6 +7,26 @@ import pandas as pd
 import numpy as np
 
 def obtener_url_decat_deproducto(url):
+    """
+    Extrae las URLs de productos de una página específica de categoría.
+
+    Esta función toma una URL de una página de categoría en un sitio web y hace una solicitud HTTP 
+    para obtener su contenido. Si la solicitud es exitosa (status code 200), utiliza BeautifulSoup 
+    para analizar el HTML y extraer los enlaces de productos que se encuentran dentro de elementos 
+    `div` con la clase `"card h-100"`. Finalmente, devuelve una lista de estos enlaces.
+
+    Parámetros:
+    -----------
+    url : str
+        La URL de la página de categoría de la cual se extraerán los enlaces de productos.
+
+    Retorna:
+    --------
+    list of str
+        Una lista de URLs (`hrefs`) de los productos encontrados en la página de categoría.
+        Si la solicitud no es exitosa, la lista devuelta estará vacía.
+
+    """
     link = requests.get(url)
     print('Hemos hecho el requests, vamos al if')
     if link.status_code == 200:
@@ -21,6 +41,25 @@ def obtener_url_decat_deproducto(url):
     return lista_href
 
 def obtener_url_decat_deproducto2(url):
+    """
+    Extrae las URLs de los enlaces de productos históricos de una página específica de categoría.
+
+    Esta función toma una URL de una página de categoría en un sitio web y hace una solicitud HTTP 
+    para obtener su contenido. Si la solicitud es exitosa (status code 200), utiliza BeautifulSoup 
+    para analizar el HTML y extraer los enlaces de productos que contienen el texto 'Histórico' 
+    dentro de elementos `div` con la clase `"card h-100"`. Finalmente, devuelve una lista de estos enlaces.
+
+    Parámetros:
+    -----------
+    url : str
+        La URL de la página de categoría de la cual se extraerán los enlaces de productos históricos.
+
+    Retorna:
+    --------
+    list of str
+        Una lista de URLs (`hrefs`) de los productos históricos encontrados en la página de categoría.
+        Si la solicitud no es exitosa, la lista devuelta estará vacía.
+    """
     link = requests.get(url)
     print('Hemos hecho el requests, vamos al if')
     if link.status_code == 200:
@@ -36,6 +75,26 @@ def obtener_url_decat_deproducto2(url):
     return lista_href
 
 def scrapeo(url):
+    """
+    Realiza el scraping de una tabla en una página web y devuelve los datos en un DataFrame.
+
+    Esta función toma una URL, realiza una solicitud HTTP para obtener su contenido, y si la solicitud 
+    es exitosa (status code 200), utiliza BeautifulSoup para analizar el HTML y extraer los datos de 
+    una tabla específica con la clase `"table table-striped table-responsive text-center"`. 
+    Los datos se organizan en un DataFrame de Pandas con las columnas obtenidas de los encabezados 
+    de la tabla.
+
+    Parámetros:
+    -----------
+    url : str
+        La URL de la página de la cual se extraerá la tabla.
+
+    Retorna:
+    --------
+    pd.DataFrame
+        Un DataFrame de Pandas con los datos extraídos de la tabla de la página web. Las columnas 
+        del DataFrame son los encabezados de la tabla en la página web.
+    """
     link = requests.get(url)
     # print('Hemos hecho el requests, vamos al if')
     if link.status_code == 200:
